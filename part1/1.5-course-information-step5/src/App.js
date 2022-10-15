130ba8d3f734007906df48c539cba5a8dec06236
Azeez Adebayo <debaycisse@gmail.com>
2022-10-15 08:56:13 +0100

    done with excercise 1.5
    I have completed excercise 1.5:
    course information, step5

---
+++ part1/1.5-course-information-step5/src/App.js
@@ -0,0 +1,68 @@
+const Header = (course) => {
+  return (
+    <div>
+      <h1>{course.title}</h1>
+    </div>
+  )
+}
+
+const Part = (part) => {
+  return (
+    <div>
+      <p>
+        {part.name} {part.excercise}
+      </p>
+    </div>
+  )
+}
+
+const Content = (allParts) => {
+  return (
+    <div>
+      <Part name={allParts.part[0].name} excercise={allParts.part[0].exercises} />
+      <Part name={allParts.part[1].name} excercise={allParts.part[1].exercises} />
+      <Part name={allParts.part[2].name} excercise={allParts.part[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (allExcercises) => {
+  return (
+    <div>
+      <p>
+        Number of exercises {allExcercises.part[0].exercises + allExcercises.part[1].exercises + allExcercises.part[2].exercises}
+      </p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises:  10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+
+  return (
+    <div>
+      <Header title={course} />
+      <Content part={parts}  />
+      <Total part={parts} />
+    </div>
+  )
+}
+
+export default App;
