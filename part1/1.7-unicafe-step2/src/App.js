592cb8db6bdff02dd8806cf8f96d317c56f5f768
Azeez Adebayo <debaycisse@gmail.com>
2022-10-16 10:37:15 +0100

    update 1.7 exercise
    I updated the source to ensure that
    components are defined outside of other
    components

--- part1/1.7-unicafe-step2/src/App.js
+++ part1/1.7-unicafe-step2/src/App.js
@@ -1,62 +1,58 @@
 import { useState } from "react";
 
+
+const Header = (props) => <h1>{props.text}</h1>
+
+const Buttons = ({onClick, text}) => <button onClick={onClick}>{text}</button> 
+
+
+const Content = (props) => <p>{props.category} {props.count}</p>
+
+const TotalFeedBack = (props) => {
+  const feedcounts = props.feedbacks
+  return (
+    <p>
+      The Total Feedbacks are : {feedcounts[0] + feedcounts[1] + feedcounts[2]}
+    </p>
+  )
+}
+
+const AverageScore = (props) => {
+  const feedcounts = props.feedbacks;
+  const averageText = "The Average Score (good: " + feedcounts[0] + ", neutral: " + feedcounts[1] + ", bad: " + feedcounts[2] + ") is : "; 
+  return (
+    <p>
+      {averageText} {(feedcounts[0] + feedcounts[1] + feedcounts[2]) / 3}
+    </p>
+  )
+}
+
+const PercentagePositiveFeed = (props) => {
+  const feedcounts = props.feedbacks;
+  const percentage = feedcounts[0] / (feedcounts[0] + feedcounts[1] + feedcounts[2]);
+  if (isNaN(percentage)){
+    return (
+      <p>
+        The percentage of positive feedback : 0%
+      </p>
+    )
+  }
+  return (
+    <p>
+      The percentage of positive feedback :  {percentage * 100}%
+    </p>
+  )
+}
+
+
 const App = () => {
-  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-
-  const Header = (props) => <h1>{props.text}</h1>
-
-  const Buttons = ({onClick, text}) => <button onClick={onClick}>{text}</button> 
-  
-
-  const Content = (props) => <p>{props.category} {props.count}</p>
-
-  const TotalFeedBack = (props) => {
-    const feedcounts = props.feedbacks
-    return (
-      <p>
-        The Total Feedbacks are : {feedcounts[0] + feedcounts[1] + feedcounts[2]}
-      </p>
-    )
-  }
-  
-  const AverageScore = (props) => {
-    const feedcounts = props.feedbacks;
-    const averageText = "The Average Score (good: " + feedcounts[0] + ", neutral: " + feedcounts[1] + ", bad: " + feedcounts[2] + ") is : "; 
-    return (
-      <p>
-        {averageText} {(feedcounts[0] + feedcounts[1] + feedcounts[2]) / 3}
-      </p>
-    )
-  }
-  
-  const PercentagePositiveFeed = (props) => {
-    const feedcounts = props.feedbacks;
-    const percentage = feedcounts[0] / (feedcounts[0] + feedcounts[1] + feedcounts[2]);
-    if (isNaN(percentage)){
-      return (
-        <p>
-          The percentage of positive feedback : 0%
-        </p>
-      )
-    }
-    return (
-      <p>
-        The percentage of positive feedback :  {percentage * 100}%
-      </p>
-    )
-  }
-
   const increaseGood = () => setGood(good + 1)
-  
   const increaseNeutral = () => setNeutral(neutral + 1)
-
   const increaseBad = () => setBad(bad + 1)  
-
-
 
   return (
     <div>
