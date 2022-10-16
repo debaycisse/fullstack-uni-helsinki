c7f59bf156539143b35e1f5bab74548ced153993
Azeez Adebayo <debaycisse@gmail.com>
2022-10-16 10:35:07 +0100

    update the app
    I have have modified the app
    to ensure that components are
    defined outside of other components
    definition block scope

--- part1/1.6-unicafe-step1/src/App.js
+++ part1/1.6-unicafe-step1/src/App.js
@@ -1,12 +1,5 @@
 import { useState } from "react";
 
-const App = () => {
-  
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-
   const Header = (props) => <h1>{props.text}</h1>
 
   const Buttons = ({onClick, text}) => <button onClick={onClick}>{text}</button> 
@@ -20,13 +13,16 @@ const App = () => {
     )
   }
 
-  const increaseGood = () => setGood(good + 1)
+
+const App = () => {
   
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  const increaseGood = () => setGood(good + 1)
   const increaseNeutral = () => setNeutral(neutral + 1)
-
   const increaseBad = () => setBad(bad + 1)  
-
-
 
   return (
     <div>
